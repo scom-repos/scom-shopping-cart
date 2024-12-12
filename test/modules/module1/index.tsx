a4991fb1d0774342dc2eea9936acfb46fc85437f
Bryan-IJS <bryan.dev@instajs.com>
2024-12-12 17:44:57 +0700

    Handle the process after checkout

--- test/modules/module1/index.tsx
+++ test/modules/module1/index.tsx
@@ -1,5 +1,9 @@
-import { Module, customModule, Container } from '@ijstech/components';
-import ScomShoppingCart, { ProductType } from '@scom/scom-shopping-cart';
+import { application, Module, customModule, Container } from '@ijstech/components';
+import { INetwork } from '@ijstech/eth-wallet';
+import { getMulticallInfoList } from '@scom/scom-multicall';
+import getNetworkList from '@scom/scom-network-list';
+import { IPaymentActivity, IPlaceOrder, ProductType } from '@scom/scom-payment-widget';
+import ScomShoppingCart from '@scom/scom-shopping-cart';
 import ScomWidgetTest from '@scom/scom-widget-test';
 
 @customModule
@@ -9,6 +13,38 @@ export default class Module1 extends Module {
 
     constructor(parent?: Container, options?: any) {
         super(parent, options);
+        const multicalls = getMulticallInfoList();
+        const networkMap = this.getNetworkMap(options.infuraId);
+        application.store = {
+            infuraId: options.infuraId,
+            multicalls,
+            networkMap
+        }
+    }
+
+    private getNetworkMap = (infuraId?: string) => {
+        const networkMap = {};
+        const defaultNetworkList: INetwork[] = getNetworkList();
+        const defaultNetworkMap: Record<number, INetwork> = defaultNetworkList.reduce((acc, cur) => {
+            acc[cur.chainId] = cur;
+            return acc;
+        }, {});
+        for (const chainId in defaultNetworkMap) {
+            const networkInfo = defaultNetworkMap[chainId];
+            const explorerUrl = networkInfo.blockExplorerUrls && networkInfo.blockExplorerUrls.length ? networkInfo.blockExplorerUrls[0] : "";
+            if (infuraId && networkInfo.rpcUrls && networkInfo.rpcUrls.length > 0) {
+                for (let i = 0; i < networkInfo.rpcUrls.length; i++) {
+                    networkInfo.rpcUrls[i] = networkInfo.rpcUrls[i].replace(/{INFURA_ID}/g, infuraId);
+                }
+            }
+            networkMap[networkInfo.chainId] = {
+                ...networkInfo,
+                symbol: networkInfo.nativeCurrency?.symbol || "",
+                explorerTxUrl: explorerUrl ? `${explorerUrl}${explorerUrl.endsWith("/") ? "" : "/"}tx/` : "",
+                explorerAddressUrl: explorerUrl ? `${explorerUrl}${explorerUrl.endsWith("/") ? "" : "/"}address/` : ""
+            }
+        }
+        return networkMap;
     }
 
     private async onShowConfig() {
@@ -39,8 +75,12 @@ export default class Module1 extends Module {
         super.init();
     }
 
-    onPaymentSuccess(value: string) {
-        console.log("onPaymentSuccess", value);
+    private async handlePlaceMarketplaceOrder(data: IPlaceOrder) {
+        console.log('handlePlaceMarketplaceOrder', data);
+    }
+
+    private async handlePaymentSuccess(data: IPaymentActivity) {
+        console.log('handlePaymentSuccess', data);
     }
 
     private onUpdateQuantity(id: string, quantity: number) {
@@ -69,14 +109,15 @@ export default class Module1 extends Module {
                         canRemove={true}
                         products={[
                             {
-                                id: '1',
+                                id: "cb772b0e-c288-a2b1-8f19-ca9ade20045d",
                                 productType: ProductType.Physical,
-                                name: 'Canon minimal',
-                                description: 'Black fujifilm DSLR camera',
-                                images: ['https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'],
-                                price: 200,
+                                name: "Denzel's Dog Treats x 6",
+                                images: ['https://images.unsplash.com/photo-1592468275155-ea8bf1f84360?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'],
+                                price: 55.99,
+                                available: 200,
                                 quantity: 1,
-                                available: 20
+                                stallId: 'b36768bc-b479-6692-94ba-d421eefbe8df',
+                                stallUri: '30018:dd2ea973537231c4c04b366acb37993a522b478c7f5705eeabef038e185605c3:b36768bc-b479-6692-94ba-d421eefbe8df'
                             },
                             {
                                 id: '2',
@@ -129,7 +170,8 @@ export default class Module1 extends Module {
                                 available: 10
                             }
                         ]}
-                        onPaymentSuccess={this.onPaymentSuccess}
+                        onPaymentSuccess={this.handlePaymentSuccess}
+                        placeMarketplaceOrder={this.handlePlaceMarketplaceOrder}
                     />
                 </i-vstack>
             </i-panel>
